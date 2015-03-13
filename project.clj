35886f91ebc25ec12f8479f558b5ccb87910f21d
David Dossot <david@dossot.net>
2015-03-13 15:13:32 -0700

    set version to 1.0.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.unbounce/treajure "1.0.0"
+(defproject com.unbounce/treajure "1.0.1-SNAPSHOT"
   :description "A treasure trove of Clojure goodness."
   :url "https://www.github.com/unbounce/treajure"
   :license {:name "The MIT License (MIT)"
