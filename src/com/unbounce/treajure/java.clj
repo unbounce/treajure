0125bf6499a5f12ebe58df94bdea78f64b03841c
James Brennan <james@jamesbrennan.ca>
2015-10-01 11:10:24 -0700

    Adds java ->clj utility function

---
+++ src/com/unbounce/treajure/java.clj
@@ -0,0 +1,20 @@
+(ns com.unbounce.treajure.java)
+
+(defprotocol ToClojure
+  (->clj [o]))
+
+(extend-protocol ToClojure
+  java.util.Map
+  (->clj [o] (let [entries (.entrySet o)]
+               (reduce (fn [m [^String k v]]
+                         (assoc m k (->clj v)))
+                       {} entries)))
+
+  java.util.List
+  (->clj [o] (vec (map ->clj o)))
+
+  java.lang.Object
+  (->clj [o] o)
+
+  nil
+  (->clj [_] nil))
