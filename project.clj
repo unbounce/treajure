f78d32735a26978b26807619bdb86decd8210fab
Roman Gonzalez <roman@unbounce.com>
2015-10-02 11:21:01 -0700

    Version 1.0.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.unbounce/treajure "1.0.7-SNAPSHOT"
+(defproject com.unbounce/treajure "1.0.7"
   :description "A treasure trove of Clojure goodness."
   :url "https://www.github.com/unbounce/treajure"
   :license {:name "The MIT License (MIT)"
