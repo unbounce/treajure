b471ab321ff506f3574eedde6638330d2525009f
Raymond Huang <raymond.huang@unbounce.com>
2016-03-15 20:14:12 -0700

    Add commonly-used map utility functions

--- src/com/unbounce/treajure/map.clj
+++ src/com/unbounce/treajure/map.clj
@@ -6,3 +6,24 @@
   (if (every? map? vals)
     (apply merge-with deep-merge vals)
     (last vals)))
+
+(defn update-if-exists
+  "Variation of clojure.core/update-in which performs no-operation if the
+  key is not present in the map. "
+  [m keys f & args]
+  (let [value (get-in m keys ::not-found)]
+    (if-not (identical? value ::not-found)
+      (assoc-in m keys (apply f value args))
+      m)))
+
+(defn map-values
+  "Create a new map of same keys and applying f to all the values.
+
+  Taken from plumatic/plumbing.
+  "
+  [m f]
+  (cond
+    (sorted? m)
+    (reduce-kv (fn [out-m k v] (assoc out-m k (f v))) (sorted-map) m)
+    :otherwise
+    (persistent! (reduce-kv (fn [out-m k v] (assoc! out-m k (f v))) (transient {}) m))))
