ab5af5330c92f8680e25307246abb12b0ce39826
Raymond Huang <raymond.huang@unbounce.com>
2020-05-21 21:04:09 -0700

    Version 1.0.13

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.unbounce/treajure "1.0.13-SNAPSHOT"
+(defproject com.unbounce/treajure "1.0.13"
   :description "A treasure trove of Clojure goodness."
   :url "https://www.github.com/unbounce/treajure"
   :license {:name "The MIT License (MIT)"
